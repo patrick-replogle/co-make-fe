c81fdb0021d14218b83e6ca706b5f0b43255fe75
patrick-replogle <patrickr1138@gmail.com>
2020-03-14 14:50:34 -0700

    started added basic file and component structure

---
+++ co-make/src/components/authentication/Register.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <input />
+        <input />
+        <input />
+        <input />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
