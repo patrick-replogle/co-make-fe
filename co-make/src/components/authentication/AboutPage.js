f026d31b94ebe602ae7489eecfa47c71ed22c4eb
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-06-24 14:45:02 -0700

    Update AboutPage.js

--- co-make/src/components/authentication/AboutPage.js
+++ co-make/src/components/authentication/AboutPage.js
@@ -13,9 +13,9 @@ const AboutPage = () => {
           <div className="aboutPageText">
             <h3>What is CoMake?</h3>
             <p>
-              CoMake is a social media app that provides local community members
+              CoMake is a social media app that provides users
               with a platform to get involved in their local community. Want to
-              call attention to an issue in your neighborhood? Would you like to
+              call attention to an issue in your neighborhood? Maybe you would like to
               volunteer your time or money towards a cause you are passionate
               about? Want to help make positive change? CoMake is here to help
               you get your concerns addressed.
